302ece90a9965f755eb5e9c0d9b2c58f51306afe
Lucas Bäckman <lucas.backman@gmail.com>
2022-06-02 18:09:29 +0300

    Add test.rb

---
+++ lib/test.rb
@@ -0,0 +1,29 @@
+# lib/test.rb
+
+require_relative 'linked_list'
+require_relative 'node'
+
+link = LinkedList.new
+
+link.append(0)
+link.prepend(1)
+link.prepend(2)
+link.append(3)
+
+puts link
+puts "Size: #{link.size}"
+
+link.pop
+link.shift
+
+puts link
+puts "Size: #{link.size}"
+
+puts "First element: #{link.at(0)}"
+
+link.insert_at(1, 10)
+link.insert_at(1, 20)
+link.insert_at(-1, 30)
+link.insert_at(-3, 50)
+
+puts link
